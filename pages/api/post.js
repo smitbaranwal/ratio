2bc1b7b081a779ed90a829ebc861417b7012457c
Smit Baranwal <smitbaranwal@gmail.com>
2023-03-19 06:27:40 +0530

    Path Updated

--- pages/api/post.js
+++ pages/api/post.js
@@ -1,5 +1,5 @@
 
-import clientPromise from "../../lib/mongodb";
+import clientPromise from "src/utils/mongodb";
 
 export default async function handler(req, res) {
   const client = await clientPromise;
