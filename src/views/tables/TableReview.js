0898726a04c76401854af4608f9f926ce6c20041
glory <baranwalglory@gmail.com>
2023-05-07 00:55:00 +0530

    sticky-column

--- src/views/tables/TableReview.js
+++ src/views/tables/TableReview.js
@@ -11,9 +11,10 @@ import TableCell, { tableCellClasses } from '@mui/material/TableCell'
 import { useEffect, useState } from 'react'
 import getTransactions from 'src/@core/utils/queries/getTransactions'
 import moment from 'moment'
-import { Account, Launch, ArrowDownThin, ArrowUpThin} from 'mdi-material-ui'
+import { Account, Launch, ArrowDownThin, ArrowUpThin } from 'mdi-material-ui'
 import BackdropLoader from 'src/@core/layouts/components/shared-components/BackdropLoader'
 import { Link, Tooltip, Typography } from '@mui/material'
+import LongText from 'src/layouts/components/subComponent/longContent'
 
 const StyledTableCell = styled(TableCell)(({ theme }) => ({
   [`&.${tableCellClasses.head}`]: {
@@ -25,6 +26,24 @@ const StyledTableCell = styled(TableCell)(({ theme }) => ({
     fontSize: 14
   }
 }))
+
+const StickyTableCell = styled(TableCell)(({ theme }) => ({
+  head: {
+    backgroundColor: theme.palette.common.black,
+    color: theme.palette.common.white,
+    left: 0,
+    position: "sticky",
+    zIndex: theme.zIndex.appBar + 2
+  },
+  body: {
+    backgroundColor: "#ddd",
+    minWidth: "50px",
+    left: 0,
+    position: "sticky",
+    zIndex: theme.zIndex.appBar + 1
+  }
+}));
+
 
 const StyledTableRow = styled(TableRow)(({ theme }) => ({
   '&:nth-of-type(odd)': {
@@ -38,17 +57,17 @@ const StyledTableRow = styled(TableRow)(({ theme }) => ({
 }))
 
 const tagStyle = {
-    padding: '6px 10px',
-    backgroundColor: 'bisque',
-    borderRadius: '24px',
-    boxShadow: '2px 2px #d3a166'
+  padding: '6px 10px',
+  backgroundColor: 'bisque',
+  borderRadius: '24px',
+  boxShadow: '2px 2px #d3a166'
 }
 
 const TableCustomized = () => {
   const [transactions, setTransactions] = useState([])
-  const [open, setOpen] = useState(false);
+  const [open, setOpen] = useState(false)
   const [page, setPage] = useState(0)
-  const [rowsPerPage, setRowsPerPage] = useState(10)
+  const [rowsPerPage, setRowsPerPage] = useState(15)
 
   const handleChangePage = (event, newPage) => {
     setPage(newPage)
@@ -61,41 +80,57 @@ const TableCustomized = () => {
 
   useEffect(() => {
     setOpen(true)
-    getTransactions(setTransactions,setOpen)
+    getTransactions(setTransactions, setOpen)
   }, [])
 
   const userAccountIcon = <Account />
 
   // const handleClose = () => {
-    
+
   // }
 
-  function isFloat(x) { return !!(x % 1); }
-
+  function isFloat(x) {
+    return !!(x % 1)
+  }
 
   return (
     <>
       <BackdropLoader open={open} />
-        <TableContainer component={Paper}>
-          <Table sx={{ minWidth: 700 }} aria-label='customized table'>
-            <TableHead>
-              <TableRow>
-                <StyledTableCell>Date (UTC)</StyledTableCell>
-                <StyledTableCell align='center'>Hash</StyledTableCell>
-                <StyledTableCell align='center'>From</StyledTableCell>
-                <StyledTableCell align='center'>To</StyledTableCell>
-                <StyledTableCell align='center'>Amount</StyledTableCell>
-                {/* <StyledTableCell align='center'>Fee</StyledTableCell>
+      <TableContainer component={Paper}>
+        <Table sx={{ minWidth: 900 }} aria-label='customized table'>
+          <TableHead>
+            <TableRow>
+            
+          
+              <StyledTableCell>Date (UTC)</StyledTableCell>
+              <StyledTableCell>Category</StyledTableCell>
+              <StyledTableCell align='center'>Hash</StyledTableCell>
+              <StyledTableCell align='center'>From</StyledTableCell>
+              <StyledTableCell align='center'>To</StyledTableCell>
+              <StyledTableCell align='center'>Amount</StyledTableCell>
+              {/* <StyledTableCell align='center'>Fee</StyledTableCell>
                 <StyledTableCell align='right'>Gain/Loss</StyledTableCell>
                 <StyledTableCell align='right'>Status</StyledTableCell> */}
-                <StyledTableCell align='center'>Tag</StyledTableCell>
-                <StyledTableCell align='center'>Description</StyledTableCell>
-              </TableRow>
-            </TableHead>
-            <TableBody>
-              {transactions.slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage).map((row, index) => (
-                
+             
+              <StyledTableCell align='center'>Description</StyledTableCell>
+              <StyledTableCell sx={{position: "sticky", right: "0", zIndex: "100"}} align='center'>Tag</StyledTableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {console.log('transactions', transactions)}
+
+            {transactions.slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage).map((row, index) =>
+              row.TokenAmount != '--' ? (
                 <StyledTableRow key={index}>
+                  
+                  <StyledTableCell scope='row'>
+                    <div style={{ display: 'inline-block', width: '100px', content: '' }}>
+                      {moment(row.Executedat, 'DD-MM-YYYY').format('ll')}
+                    </div>
+
+                    {/* {moment(row.Executedat, 'DD-MM-YYYY hh:mm:ss T').format('DD/MM/YYYY hh:mm a')} */}
+                  </StyledTableCell>
+
                   <StyledTableCell scope='row'>
                     <div style={{ display: 'inline-block', width: '150px', content: '' }}>
                       {row.Type === 'Outgoing' ? (
@@ -108,73 +143,69 @@ const TableCustomized = () => {
                       <span style={{ verticalAlign: 'super' }}> {row.Category} </span>
                     </div>
                     <br />
-                    {/* {moment(row.Executedat, 'DD-MM-YYYY hh:mm:ss T').format('DD/MM/YYYY hh:mm a')} */}
-                    {moment(row.Executedat, 'DD-MM-YYYY').format('ll')}
                   </StyledTableCell>
+
                   <StyledTableCell scope='row'>
-                    <div style={{ display: 'inline-block', width: '150px', content: '', textAlign: "center" }}>
-                    <span
-                                    variant="button"
-                                    color="white"
-                                    lineheight="1"
-                                  >
-                                    <Tooltip title={row.TransactionHash}>
-                                     <Link href={`https://etherscan.io/tx/${row.TransactionHash}`} target='_blank'>
-                                    {row.TransactionHash.length > 5
-                                        ? `${row.TransactionHash.substring(
-                                            0,
-                                            5
-                                          )}...`
-                                        : ( row.TransactionHash)}
-                                      </Link>
-                                      </Tooltip>
-                                  </span>
-                    </div>
-                  </StyledTableCell>
-                  <StyledTableCell size='small' align='right'>
-                    <div style={{ width: '180px' }}>
-                      {row.FromAddress.length > 3 ? userAccountIcon : ''}
-                      <span style={{ verticalAlign: 'super' }}>
-                        {row.FromAddress.length > 3
-                          ? row.FromAddress.substring(0, 8) + '...' + row.FromAddress.substring(36, 42)
-                          : '--'}
+                    <div style={{ display: 'inline-block', width: '120px', content: '', textAlign: 'center' }}>
+                      <span variant='button' color='white' lineheight='1'>
+                        <Tooltip title={row.TransactionHash}>
+                          <>
+                          <Link href={`https://etherscan.io/tx/${row.TransactionHash}`} target='_blank'>
+                            <span style={{ fontSize: '0.8rem' }}>
+                              <Launch sx={{color: "#607de6"}}/>
+                            </span>
+                            </Link>
+                            <span style={{ verticalAlign: 'super' }}>
+                              {row.TransactionHash.length > 3
+                                ? row.TransactionHash.substring(0, 4) + '...' + row.TransactionHash.substring(38, 42)
+                                : '--'}
+                            </span>
+                            </>
+                        </Tooltip>
                       </span>
                     </div>
                   </StyledTableCell>
-                  <StyledTableCell align='right'>
-                    <div style={{ width: '180px' }}>
+                  <StyledTableCell size='small' align='center'>
+                    <div style={{ width: '120px' }}>
                       {row.FromAddress.length > 3 ? userAccountIcon : ''}
                       <span style={{ verticalAlign: 'super' }}>
-                        {row.ToAddress.length > 3
-                          ? row.ToAddress.substring(0, 8) + '...' + row.ToAddress.substring(36, 42)
+                        {row.FromAddress.length > 3
+                          ? row.FromAddress.substring(0, 4) + '...' + row.FromAddress.substring(38, 42)
                           : '--'}
                       </span>
                     </div>
                   </StyledTableCell>
                   <StyledTableCell align='center'>
-                    {console.log("row", row)}
-                    {/* <div>{row.TokenAmount !== '--' ? '$' + row.TokenAmount : '--'}</div> */}
-                    <div style={{ width: '180px' }}>
+                    <div style={{ width: '120px' }}>
+                      {row.FromAddress.length > 3 ? userAccountIcon : ''}
                       <span style={{ verticalAlign: 'super' }}>
-                      {row.TokenAmount == "--" ?(
-                        ''
-                      ) : isFloat(row.TokenAmount) == true ? (
-                        row.USDAmount != "--" ? 
-                       (
-                        <Typography>
-
-                        {parseFloat(row.TokenAmount).toFixed(3)} BANK (${row.USDAmount})
-                        </Typography>) : (
+                        {row.ToAddress.length > 3
+                          ? row.ToAddress.substring(0, 4) + '...' + row.ToAddress.substring(38, 42)
+                          : '--'}
+                      </span>
+                    </div>
+                  </StyledTableCell>
+                  <StyledTableCell align='center'>
+                    {/* <div>{row.TokenAmount !== '--' ? '$' + row.TokenAmount : '--'}</div> */}
+                    <div style={{ width: '200px' }}>
+                      <span style={{ verticalAlign: 'super' }}>
+                        {row.TokenAmount == '--' ? (
+                          ''
+                        ) : isFloat(row.TokenAmount) == true ? (
+                          row.USDAmount != '--' ? (
+                            <Typography>
+                              {parseFloat(row.TokenAmount).toFixed(3)} BANK (${row.USDAmount})
+                            </Typography>
+                          ) : (
+                            <Typography>
+                              {parseFloat(row.TokenAmount).toFixed(3)} BANK (${row.USDAmount})
+                            </Typography>
+                          )
+                        ) : (
                           <Typography>
-                           {parseFloat(row.TokenAmount).toFixed(3)} BANK (${row.USDAmount})
+                            {row.TokenAmount} BANK (${row.USDAmount})
                           </Typography>
-                        )
-                      ):
-                      <Typography>{row.TokenAmount} BANK (${row.USDAmount})</Typography> 
-                    
-                      }
-                                           
-                      
+                        )}
                       </span>
                     </div>
                   </StyledTableCell>
@@ -187,20 +218,38 @@ const TableCustomized = () => {
                   {/* <StyledTableCell align='right'>
                   <div style={{ color: row.TransactionStatus === 'Success' ? 'green' : 'red' }}>{row.TransactionStatus}</div>
                   </StyledTableCell> */}
-                  <StyledTableCell align='center'>
-                    {
-                      row.Tag && row.Tag !== '--' ? <span style={tagStyle}>{row.Tag}</span> : ''
-                    }
-                  </StyledTableCell>
+                  
 
-                  <StyledTableCell align='center'>{row.Description}</StyledTableCell>
+                  <StyledTableCell align='center'>
+                    <div style={{ width: '180px' }}>
+                      <LongText content={row.Description} limit={10} />
+                    </div>
+                  </StyledTableCell>
+                  <StyledTableCell align='center' sx={{position: "sticky", right: "0", zIndex: "100", backgroundColor: "#fff"}}>
+                  <div style={{ display: 'inline-block', width: '150px', content: '' }}>
+                    {row.Tag && row.Tag !== '--' ?  <span style={{ verticalAlign: 'super' }}>{row.Tag}</span> : ''}
+                    </div>
+                    {/* <div style={{ display: 'inline-block', width: '150px', content: '' }}>
+                      {row.Type === 'Outgoing' ? (
+                        <ArrowUpThin style={{ color: '#f44336' }} />
+                      ) : row.Type === 'Incoming' ? (
+                        <ArrowDownThin style={{ color: '#4caf50' }} />
+                      ) : (
+                        ''
+                      )}
+                      <span style={{ verticalAlign: 'super' }}> {row.Category} </span>
+                    </div> */}
+                  </StyledTableCell>
                 </StyledTableRow>
-              ))}
-            </TableBody>
-          </Table>
-        </TableContainer>
-        <TablePagination
-        rowsPerPageOptions={[10, 25, 100]}
+              ) : (
+                ''
+              )
+            )}
+          </TableBody>
+        </Table>
+      </TableContainer>
+      <TablePagination
+        rowsPerPageOptions={[15, 30, 100]}
         component='div'
         count={transactions.length}
         rowsPerPage={rowsPerPage}
