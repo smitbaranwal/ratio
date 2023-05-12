93c21e853a8cf42b5d96c7f071fa383fe0e843a0
Smit Baranwal <smitbaranwal@gmail.com>
2023-05-13 01:59:28 +0530

    date range filter

---
+++ src/@core/layouts/components/shared-components/BasicDateRangePicker.js
@@ -0,0 +1,22 @@
+import * as React from 'react';
+import { DemoContainer } from '@mui/x-date-pickers/internals/demo';
+import { LocalizationProvider } from '@mui/x-date-pickers-pro';
+import { AdapterDayjs } from '@mui/x-date-pickers-pro/AdapterDayjs';
+import { DateRangePicker } from '@mui/x-date-pickers-pro/DateRangePicker';
+
+export default function BasicDateRangePicker(props) {
+    const {updateDate} = props
+    function updateDaten(newValue){
+        updateDate(newValue)
+    }
+
+  return (
+    <LocalizationProvider dateAdapter={AdapterDayjs}>
+      <DemoContainer components={['DateRangePicker']}>
+        <DateRangePicker localeText={{ start: 'Start Date', end: 'End Date' }} 
+        onChange={(newValue) => updateDaten(newValue)}
+         />
+      </DemoContainer>
+    </LocalizationProvider>
+  );
+}
