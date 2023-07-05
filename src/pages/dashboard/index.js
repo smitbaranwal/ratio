ce010c77e418ce122f64d0b114c048da192a6d49
Smit Baranwal <smitbaranwal@gmail.com>
2023-07-05 11:59:18 +0530

    category wise dashboard

--- src/pages/dashboard/index.js
+++ src/pages/dashboard/index.js
@@ -18,19 +18,30 @@ import Table from 'src/views/dashboard/Table'
 import DepositWithdraw from 'src/views/dashboard/DepositWithdraw'
 import SalesByCountries from 'src/views/dashboard/SalesByCountries'
 import AsofdateChart from 'src/views/dashboard/AsofdateChart'
+import CategoryWiseProfitLoss from 'src/views/dashboard/CategoryWiseProfitLoss'
 
 const Dashboard = () => {
   return (
     <ApexChartWrapper>
       <Grid container spacing={6}>
+
+      
+
         <Grid item xs={12} md={12} lg={12}>
           {/* <SalesByCountries /> */}
+          {/* <CategoryWiseProfitLoss /> */}
           <AsofdateChart />
+        </Grid>
+
+        <Grid item xs={12} md={12} lg={12}>
+          <CategoryWiseProfitLoss />
         </Grid>
        
         <Grid item xs={12}>
           <Table />
         </Grid>
+
+        
 
         {/* <Grid item xs={12} md={12} lg={12}>
           <DepositWithdraw />
